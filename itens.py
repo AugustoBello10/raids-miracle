52cea56cc52cb4b325e9369ba27f256b82fef289
AugustoBello10 <97684580+AugustoBello10@users.noreply.github.com>
2026-01-22 17:36:29 -0400

    Update itens.py

--- itens.py
+++ itens.py
@@ -64,34 +64,6 @@ MINING_PICKS = {
     "Enhanced Pick":        {"bonus_break": 6, "bonus_collect": 1.00}
 }
 
-ALCHEMY_MENU_CATS = {
-    "cat_atk": ["Sudden Death", "Explosion", "Great Fireball", "Heavy Magic Missile", "Light Magic Missile", "Fireball", "Ultimate Healing", "Intense Healing", "Antidote Rune", "Envenom"],
-    "cat_sup": ["Paralyze", "Magic Wall", "Energy Bomb", "Energy Wall", "Energy Field", "Fire Bomb", "Fire Wall", "Fire Field", "Poison Bomb", "Poison Wall", "Poison Field", "Soulfire", "Animate Dead", "Convince Creature", "Chameleon", "Desintegrate", "Destroy Field"]
-}
-
-RECEITAS = {
-    "Giant Ruby": { "multiplicador": 5, "ingredientes": {"Small Ruby": 10}, "perde_na_falha": True },
-    "Giant Emerald": { "multiplicador": 5, "ingredientes": {"Small Emerald": 10}, "perde_na_falha": True },
-    "Giant Sapphire": { "multiplicador": 5, "ingredientes": {"Small Sapphire": 10}, "perde_na_falha": True },
-    "Giant Amethyst": { "multiplicador": 5, "ingredientes": {"Small Amethyst": 10}, "perde_na_falha": True },
-    "Modified Pick": { "multiplicador": 2, "ingredientes": {"Pick": 1, "Steel": 5}, "perde_na_falha": True },
-    "Advanced Pick": { "multiplicador": 1.5, "ingredientes": {"Pick": 1, "Steel": 10}, "perde_na_falha": True },
-    "Enhanced Pick": { "multiplicador": 1, "ingredientes": {"Pick": 1, "Steel": 20}, "perde_na_falha": True },
-    "Diamond Knife": { "multiplicador": 1, "ingredientes": {"Combat Knife": 1, "Small Diamond": 10, "Hell Steel": 5}, "perde_na_falha": True },
-    "Ember Rune": { "multiplicador": 1, "ingredientes": {"Ember Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Protector Rune": { "multiplicador": 1, "ingredientes": {"Protector Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Obsidian Rune": { "multiplicador": 1, "ingredientes": {"Obsidian Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Aegis Rune": { "multiplicador": 1, "ingredientes": {"Aegis Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Astral Rune": { "multiplicador": 1, "ingredientes": {"Astral Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Molten Rune": { "multiplicador": 1, "ingredientes": {"Molten Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Fiery Stone": { "multiplicador": 0.5, "ingredientes": {"Glimmering Soil": 5}, "perde_na_falha": True },
-    "Reinforced Rod": { "multiplicador": 2, "ingredientes": {"Fishing Rod": 1, "Steel": 5}, "perde_na_falha": True },
-    "Engineered Fishing Rod": { "multiplicador": 1.5, "ingredientes": {"Fishing Rod": 1, "Steel": 10, "Draconian Steel": 1}, "perde_na_falha": True },
-    "Volcanic Fishing Rod": { "multiplicador": 1, "ingredientes": { "Fishing Rod": 1, "Steel": 20, "Glimmering Soil": 10, "Draconian Steel": 5, "Hell Steel": 3 }, "perde_na_falha": True },
-    "Golden Fishing Rod": { "multiplicador": 0.5, "ingredientes": { "Fishing Rod": 1, "Steel": 40, "Draconian Steel": 10, "Gold Ingot": 3, "Hell Steel": 3 }, "perde_na_falha": True },
-    "10x Steel Bolts": { "multiplicador": 0.4, "ingredientes": {"Bolt": 5, "Steel": 1, "Natural Soil": 1}, "perde_na_falha": True },
-}
-
 ESTRUTURA_MENU = {
     "crafting": {
         "relics": ["Giant Ruby", "Giant Emerald", "Giant Sapphire", "Giant Amethyst"],
@@ -112,8 +84,8 @@ RASHID_SCHEDULE = {
     6: {"city": "Carlin", "desc": "Primeiro andar do DP.", "url": "https://tibiamaps.io/map#32334,31782,6:3"}
 }
 
-# --- URLs WIKI ---
-WIKI_MONSTER_URL = "https://tibia.fandom.com/wiki/"
+# --- URLs WIKI (CORRIGIDAS) ---
+WIKI_MONSTER_URL = "https://www.tibiawiki.com.br/wiki/"
 BASE_MIRACLE_URL = "https://miracle74.com/"
 MIRACLE_ITEMS_URL = "https://miracle74.com/?subtopic=items"
 MIRACLE_ITEM_ID_URL = "https://miracle74.com/?subtopic=items&id="
