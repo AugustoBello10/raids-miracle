3753f01e63e9659035856e4df94cefbaac40ca38
AugustoBello10 <97684580+AugustoBello10@users.noreply.github.com>
2026-01-23 17:11:58 -0400

    Update itens.py

--- itens.py
+++ itens.py
@@ -14,7 +14,7 @@ ARMAS_TREINO = {
 
 ALCHEMY_DATA = { "converter": { "charges": 100, "cost": 500, "base_chance": 10, "skill_factor": 0.2 }, "crystals": { "Spark Crystal": {"base_chance": 20}, "Lightning Crystal": {"base_chance": 15}, "Inferno Crystal": {"base_chance": 10} } }
 
-ALCHEMY_RUNES = { "Sudden Death": {"min": 55, "base": 22.72, "pro": True}, "Explosion": {"min": 28, "base": 27.77, "pro": True}, "Great Fireball": {"min": 22, "base": 41.66, "pro": False}, "Heavy Magic Missile": {"min": 13, "base": 71.42, "pro": False}, "Light Magic Missile": {"min": 10, "base": 100.0, "pro": False}, "Fireball": {"min": 16, "base": 83.33, "pro": False}, "Ultimate Healing": {"min": 22, "base": 50.0, "pro": True}, "Intense Healing": {"min": 13, "base": 83.33, "pro": False} }
+ALCHEMY_RUNES = { "Sudden Death": {"min": 55, "base": 22.72}, "Explosion": {"min": 28, "base": 27.77}, "Great Fireball": {"min": 22, "base": 41.66}, "Heavy Magic Missile": {"min": 13, "base": 71.42}, "Light Magic Missile": {"min": 10, "base": 100.0}, "Fireball": {"min": 16, "base": 83.33}, "Ultimate Healing": {"min": 22, "base": 50.0}, "Intense Healing": {"min": 13, "base": 83.33} }
 
 MINING_PICKS = { "Pick (Normal)": {"bonus_break": 0, "bonus_collect": 0.0}, "Modified Pick": {"bonus_break": 2, "bonus_collect": 0.25}, "Advanced Pick": {"bonus_break": 4, "bonus_collect": 0.50}, "Enhanced Pick": {"bonus_break": 6, "bonus_collect": 1.00} }
 
@@ -27,33 +27,21 @@ RECEITAS = {
     "Advanced Pick": { "multiplicador": 1.5, "ingredientes": {"Pick": 1, "Steel": 10}, "perde_na_falha": True },
     "Enhanced Pick": { "multiplicador": 1, "ingredientes": {"Pick": 1, "Steel": 20}, "perde_na_falha": True },
     "Diamond Knife": { "multiplicador": 1, "ingredientes": {"Combat Knife": 1, "Small Diamond": 10, "Hell Steel": 5}, "perde_na_falha": True },
-    "Ember Rune": { "multiplicador": 1, "ingredientes": {"Ember Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Protector Rune": { "multiplicador": 1, "ingredientes": {"Protector Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Obsidian Rune": { "multiplicador": 1, "ingredientes": {"Obsidian Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Aegis Rune": { "multiplicador": 1, "ingredientes": {"Aegis Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Astral Rune": { "multiplicador": 1, "ingredientes": {"Astral Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Molten Rune": { "multiplicador": 1, "ingredientes": {"Molten Fragment": 5, "Pulverized Ore": 3, "Onyx": 1}, "nao_perde": ["Onyx"] },
-    "Fiery Stone": { "multiplicador": 0.5, "ingredientes": {"Glimmering Soil": 5}, "perde_na_falha": True },
     "Reinforced Rod": { "multiplicador": 2, "ingredientes": {"Fishing Rod": 1, "Steel": 5}, "perde_na_falha": True },
     "Engineered Fishing Rod": { "multiplicador": 1.5, "ingredientes": {"Fishing Rod": 1, "Steel": 10}, "perde_na_falha": True },
-    "Volcanic Fishing Rod": { "multiplicador": 1, "ingredientes": { "Fishing Rod": 1, "Steel": 20, "Glimmering Soil": 10}, "perde_na_falha": True },
-    "Golden Fishing Rod": { "multiplicador": 0.5, "ingredientes": { "Fishing Rod": 1, "Steel": 40, "Draconian Steel": 10, "Gold Ingot": 3}, "perde_na_falha": True },
+    "Volcanic Fishing Rod": { "multiplicador": 1, "ingredientes": {"Fishing Rod": 1, "Steel": 20, "Glimmering Soil": 10}, "perde_na_falha": True },
     "10x Steel Bolts": { "multiplicador": 0.4, "ingredientes": {"Bolt": 5, "Steel": 1}, "perde_na_falha": True },
 }
 
 ESTRUTURA_MENU = {
     "crafting": {
         "relics": ["Giant Ruby", "Giant Emerald", "Giant Sapphire", "Giant Amethyst"],
-        "runes": ["Aegis Rune", "Astral Rune", "Ember Rune", "Molten Rune", "Obsidian Rune", "Protector Rune", "Fiery Stone"],
-        "tools": ["Advanced Pick", "Diamond Knife", "Enhanced Pick", "Modified Pick"],
-        "fishing": ["Engineered Fishing Rod", "Golden Fishing Rod", "Reinforced Rod", "Volcanic Fishing Rod"],
+        "tools": ["Advanced Pick", "Modified Pick", "Enhanced Pick", "Diamond Knife"],
+        "fishing": ["Reinforced Rod", "Engineered Fishing Rod", "Volcanic Fishing Rod"],
         "ammo": ["10x Steel Bolts"]
     }
 }
 
-ALCHEMY_MENU_CATS = { "cat_atk": ["Sudden Death", "Explosion", "Great Fireball", "Heavy Magic Missile", "Light Magic Missile", "Fireball", "Ultimate Healing", "Intense Healing", "Envenom"], "cat_sup": ["Paralyze", "Magic Wall", "Energy Bomb", "Energy Wall", "Fire Bomb", "Fire Wall", "Poison Bomb", "Poison Wall", "Soulfire", "Animate Dead", "Convince Creature", "Chameleon", "Desintegrate", "Destroy Field"] }
-
-# URLS E LINKS (V22 + V37)
 RASHID_SCHEDULE = {
     0: {"city": "Thais", "desc": "Ao lado direito do DP.", "url": "https://tibiamaps.io/map#32359,32226,6:3"},
     1: {"city": "Venore", "desc": "Loja ao sul.", "url": "https://tibiamaps.io/map#32945,32110,6:3"},
